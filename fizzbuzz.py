d53f34d98a2a94d581524af4aaf980a6aef7532a
Claudia Beresford <cberesford@pivotal.io>
2019-06-07 18:22:46 +0100

    part 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,8 @@
 def is_divisible_by_three(number):
      return number % 3 == 0
+
+def is_divisible_by_five(number):
+     return number % 5 == 0
+
+def is_divisible_by_three_and_five(number):
+     return number % 15 == 0
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -5,3 +5,15 @@ def test_that_number_is_divisible_by_three():
 
 def test_that_number_is_NOT_divisible_by_three():
     assert fizzbuzz.is_divisible_by_three(1) == False
+
+def test_that_number_is_divisible_by_five():
+    assert fizzbuzz.is_divisible_by_five(5) == True
+
+def test_that_number_is_NOT_divisible_by_five():
+    assert fizzbuzz.is_divisible_by_five(1) == False
+
+def test_that_number_is_divisible_by_three_and_five():
+    assert fizzbuzz.is_divisible_by_three_and_five(15) == True
+
+def test_that_number_is_NOT_divisible_by_three_and_five():
+    assert fizzbuzz.is_divisible_by_three_and_five(1) == False
